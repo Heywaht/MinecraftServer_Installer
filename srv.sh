60c344072fc4975662a2b52230affcd3e5c913e5
Heywaht <62307852+Heywaht@users.noreply.github.com>
2021-11-22 12:18:39 +0100

    Update srv.sh

--- srv.sh
+++ srv.sh
@@ -8,5 +8,6 @@ mkdir MinecraftServer1.16.5
 cd MinecraftServer1.16.5
 wget https://raw.githubusercontent.com/Heywaht/mc-rlsrv-1.16.5/main/eula.txt
 wget https://api.pl3x.net/v2/purpur/1.16.5/1171/server.jar
-java -Xmx2000M -Xms2000M -jar server.jar nogui
+apt install screen
+screen java -Xmx2000M -Xms2000M -jar server.jar nogui
 echo "Your minecraft server on 1.16.5 is now running !"
